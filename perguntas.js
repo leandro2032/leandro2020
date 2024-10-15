b83c04b508bbdd2c99a317396d4ef7850efb9d77
leandro2032 <168580906+leandro2032@users.noreply.github.com>
2024-10-15 15:08:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Programação',
+    'O que é roblox?',
+    'e um jogo que voce ode gerar a sua vid inteira virar uma noiado que fica  dia todo no celular mas tambem pode gerar felicidade'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da brasil',
+    'A capital do brasil é brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz rato em Inglês?',
+    'rato em ingles é mouse (mause)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz rato em Inglês?',
+    'rato em ingles mouse (mause)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz rato em Inglês?',
+    'rato em ingles mouse (mause)'
+)
